0877d6397cdd6771d91a7adcc742122126d3ffce
alperentankut <tankut.alperen@gmail.com>
2022-05-09 13:17:58 +0300

    go updated

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,22 @@
+//package clause
+//Bizim go da yazdığımız her kod her zaman paketler halindedir. Biz kodları
+//paketler halinde yazarak daha modüler hale getiririz. Daha derli toplu
+//olur. Bizim burda yazacağımız bütün kodlar main paketi içerisinde olacak
+
 package main
 
+//Import statement
+//Bazı durumlarda kendi paketimiz dışında başkalarının paketlerini veya
+//go tarafından hazırlanmış hazır paketleri kullanırız.
+
 import "fmt"
+
+//My code
 
 func main(){
 	fmt.Println("Hello world")
 }
+
+//go da bir program main paketinin içindeki main fonksiyonundan başlar
+//bundan dolayı fonksiyonu main ile yazdık.
+//build komutunu yazdığımızda ise bize .exe uzantılı bir dosya veriyor.
