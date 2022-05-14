927abfa887358e4c2f942707df86b668e1959e55
alperentankut <tankut.alperen@gmail.com>
2022-05-14 22:49:06 +0300

    go update

--- 02_variable_declaration/main.go
+++ 02_variable_declaration/main.go
@@ -2,9 +2,26 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
-	var name string = "Alperen"
+	// var name string = "Alperen"
 
-	fmt.Println("Hello",name)
+	// fmt.Println("Hello",name)
+
+	// var name string = "Alperen" 
+
+	name := "Alperen" //Burda değişken tipini belirtmeden
+	//direk değeri vermek için := ifadesini kullanırız.
+
+	var age int = 26
+
+	var isMarried bool = true
+
+	var firstName , lastName string = "Alperen" , "Tankut"
+
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+	fmt.Println(firstName , lastName)
 }
