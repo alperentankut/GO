5eea9a754687d25df8e90177c38570b9107f6857
alperentankut <tankut.alperen@gmail.com>
2023-01-20 10:21:52 +0300

    print added

---
+++ 10_print_printf_println/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Print("Merhaba")
+	fmt.Printf("Merhaba")
+	fmt.Println("Merhaba")
+
+}
+
+//Println kodumuzu yazdırdıktan sonra yeni bir satır oluşturur. Print ve
+//Printf aralarındaki temel fark budur.
